7148b80a13d3086ac046e310a1322bc96a2effbe
François Chapuis <afnarel@gmail.com>
2014-01-14 20:14:28 +0100

    Updated the autostart script so that if an application is already started it is not started twice

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,3 +1,18 @@
-firefox &
-skype &
-sylpheed &
+launch() {
+    app=$1
+    # If the app is not already started
+    # start it
+    ps -C "$app" > /dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+        $app &
+    fi
+}
+
+launch firefox
+launch skype
+launch sylpheed
+
+#firefox &
+#skype &
+#sylpheed &
