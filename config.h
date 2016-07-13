cc0c444d0e488a8d0c3bf002782f2ec0e0b7351f
François Chapuis <afnarel@gmail.com>
2016-07-13 10:07:17 +0200

    Replace l with p for resizing windows to avoid conflict with Window's logout shortcut when running inside a virtual machine

--- config.h
+++ config.h
@@ -81,7 +81,6 @@ static const char *termcmd[]  = { "urxvt", NULL }; // xterm, uxterm, terminology
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_exclam, spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -89,7 +88,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_p,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
